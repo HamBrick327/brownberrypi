87df81de963226930f0ede693da5f4bddae3404c
HamBrick327 <derpykidgamer@gmail.com>
2025-05-20 22:18:03 -0600

    code works... probably, currently at a bug that may be either hardware or software

--- keeb.py
+++ keeb.py
@@ -1,63 +1,10 @@
 ## TODO test this
-import digitalio
 import board
+import digitalio
+from time import sleep
 
-pin = Pin("LED", Pin.OUT)
+# pin = Pin("LED", Pin.OUT)
 
-rowPins = [0, 1, 2, 3]
-for pin in rowPins:
-    temp = []
-    exec(f'temp.append(digitalio.DigitialInOut(board.d{pin}))')
-    rowPins = temp
-
-colPins = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-for pin in colPins:
-    temp = []
-    exec(f'temp.append(digitalio.DigitialInOut(board.d{pin}))')
-    colPins = temp
-
-
-outputPins = []
-inputPins = []
-liveCols = []
-liveRows = []
-''' EXAMPLE PIN CODE
-pin_led = Pin(16, mode=Pin.OUT, value=1)
-pin_led = Pin(16, mode=Pin.OUT, value=0)
-pin_14 = Pin(14, mode=Pin.IN)
-'''
-
-## check columns
-for pin in rowPins:
-    exec(f'''{pin}.direction = digitalio.Direction.OUTPUT
-        {pin}.value = True
-         ''')
-
-for index, pin in enumerate(colPins):
-    # for every pin, define it as an input with a default high value
-    exec(f'''{pin}.direction = digitalio.Direction.INPUT
-    {pin}.pull = digitalio.Pull.DOWN''') # TODO finish this affront to mankind
-
-    if inputPins[index].value():
-        liveCols.append(pin)
-
-##check rows
-for pin in colPins:
-    exec(f'''{pin}.direction = digitalio.Direction.OUTPUT
-        {pin}.value = True
-         ''')
-
-for index, pin in enumerate(rowPins):
-    # for every pin, define it as an input with a default high value
-    exec(f'''{pin}.direction = digitalio.Direction.INPUT
-    {pin}.pull = digitalio.Pull.DOWN''') # TODO finish this affront to mankind
-
-    if inputPins[index].value():
-        liveRows.append(pin)
-## TODO add keybind logic
-## TODO add shift logic
-## TODO add shift + fn1 logic for shift + numkeys
 keybinds = [[['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
             ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
             ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
@@ -66,6 +13,72 @@ keybinds = [[['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
              ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
              ['', '', '', '', '', '', '', '', '', '']
             ]]
+
+## setup
+rowPins = [0, 1, 2, 3]
+temp = []
+for pin in rowPins:
+    #temp = digitalio.DigitalInOut((board.GP0))
+    #temp.append(digitalio.DigitalInOut(board.{pin}))
+    exec(f'temp.append(digitalio.DigitalInOut(board.GP{pin}))')
+    rowPins = temp
+
+colPins = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+temp = []
+for pin in colPins:
+    exec(f'temp.append(digitalio.DigitalInOut(board.GP{pin}))')
+    colPins = temp
+
+
+outputPins = []
+inputPins = []
+liveCols = []
+liveRows = []
+
+## end setup
+while True:
+    liveCols = []
+    liveRows = []   ## check for live columns
+    ## set the rows as outputs
+    for pin in rowPins:
+        pin.direction = digitalio.Direction.OUTPUT
+        pin.value = True ## this should have the same functionality
+
+    ## set the columns as inputs
+    for index, pin in enumerate(colPins):
+        inputPins = colPins
+        pin.direction = digitalio.Direction.INPUT
+        pin.pull = digitalio.Pull.DOWN
+        # for every pin, define it as an input with a default low value
+
+        if inputPins[index].value:
+            liveCols.append(index)
+
+    sleep(.02)
+    ##check rows
+    ## set columns as outputs
+    for pin in colPins:
+        outputPins = colPins
+        pin.direction = digitalio.Direction.OUTPUT
+        pin.value = True ## this should have the same functionality
+
+    ## set the rowPins as inputs
+    for index, pin in enumerate(rowPins):
+        inputPins = rowPins
+        pin.direction = digitalio.Direction.INPUT
+        pin.pull = digitalio.Pull.DOWN
+ 
+        if inputPins[index].value:
+            liveRows.append(index)
+            
+    print('row', liveRows)
+    print('col', liveCols)
+
+    sleep(.02)
+## TODO add keybind logic
+## TODO add shift logic
+## TODO add shift + fn1 logic for shift + numkeys
+
 
 ''' begin seudocode
 if fn1 is pressed:
